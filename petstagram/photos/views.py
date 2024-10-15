8bc4baacfc50d58c0fa2b75d2bbdfd0c8148c59d
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-10-15 19:55:17 +0300

    Add delete form for photo

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -37,6 +37,11 @@ def photo_edit_page(request, pk: int):
     return render(request, 'photos/photo-edit-page.html', context)
 
 
+def photo_delete(request, pk: int):
+    Photo.objects.get(pk=pk).delete()
+    return redirect('home')
+
+
 def photo_details_page(request, pk: int):
     photo = Photo.objects.get(pk=pk)
     likes = photo.like_set.all()
