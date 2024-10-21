340d02b799eb51ef657edf0ee3008c6976894e0a
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-10-22 00:28:55 +0300

    Add comment section in photos/views to see how to transform FBV with CBV

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -58,3 +58,38 @@ def photo_details_page(request, pk: int):
     }
 
     return render(request, 'photos/photo-details-page.html', context)
+
+
+""" CBV representation for all FBV above:
+
+class PhotoAddPage(CreateView):
+    model = Photo
+    template_name = 'photos/photo-add-page.html'
+    form_class = PhotoAddForm
+    success_url = reverse_lazy('home')
+
+
+class PhotoEditPage(UpdateView):
+    model = Photo
+    form_class = PhotoEditForm
+    template_name = 'photos/photo-edit-page.html'
+
+    def get_success_url(self):
+        return reverse_lazy('photo-details', kwargs={'pk': self.object.pk})
+
+
+class PhotoDetailsView(DetailView):
+    model = Photo
+    template_name = 'photos/photo-details-page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['likes'] = self.object.like_set.all()
+        context['comments'] = self.object.comment_set.all()
+        context['comment_form'] = CommentForm()
+
+        return context
+
+
+"""
