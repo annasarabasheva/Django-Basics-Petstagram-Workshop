6b2f14ceb63c26d5459419a8c5009567c5799c8a
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-10-21 22:08:13 +0300

    Replace FBV pet_add_page with CBV named PetAddPage

--- petstagram/pets/urls.py
+++ petstagram/pets/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from petstagram.pets import views
 
 urlpatterns = [
-    path('add/', views.pet_add_page, name='add-pet'),
+    path('add/', views.PetAddPage.as_view(), name='add-pet'),
     path('<str:username>/pet/<slug:pet_slug>/', include([
         path('', views.pet_details_page, name='pet-details'),
         path('edit/', views.pet_edit_page, name='edit-pet'),
--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
+
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from petstagram.common.forms import CommentForm
 from petstagram.pets.models import Pet
 from petstagram.pets.forms import PetAddForm, PetEditForm, PetDeleteForm
 
+""" FBV:
 
 def pet_add_page(request):
     form = PetAddForm(request.POST or None)
@@ -21,6 +24,14 @@ def pet_add_page(request):
     }
 
     return render(request, 'pets/pet-add-page.html', context)
+"""
+
+
+class PetAddPage(CreateView): # CBV version of pet_add_page view
+    model = Pet
+    form_class = PetAddForm
+    template_name = 'pets/pet-add-page.html'
+    success_url = reverse_lazy('profile-details', kwargs={'pk': 1})
 
 
 def pet_edit_page(request, username: str, pet_slug: str):
