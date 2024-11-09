2e870a7ca68d68c74109518110689eecaf067d20
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-11-09 19:42:26 +0200

    Create Logout functionality for users

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 from petstagram.accounts import views
 from petstagram.accounts.views import AppUserLoginView, AppUserRegisterView
@@ -5,6 +6,7 @@ from petstagram.accounts.views import AppUserLoginView, AppUserRegisterView
 urlpatterns = [
     path('login/', AppUserLoginView.as_view(), name='login'),
     path('register/', AppUserRegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/<int:pk>/', include([
         path('', views.profile_details, name='profile-details'),
         path('edit/', views.profile_edit, name='profile-edit'),
