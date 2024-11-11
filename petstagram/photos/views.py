d77c1e285e448436ba54f2419302907cfa7b3ddb
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-11-11 17:51:06 +0200

    Add UserPassesTestMixin in Photo edit and delete view

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DetailView
 
@@ -22,10 +22,14 @@ class PhotoAddPage(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PhotoEditPage(LoginRequiredMixin, UpdateView):
+class PhotoEditPage(LoginRequiredMixin,  UserPassesTestMixin, UpdateView):
     model = Photo
     form_class = PhotoEditForm
     template_name = 'photos/photo-edit-page.html'
+
+    def test_func(self):
+        photo = get_object_or_404(Photo, slug=self.kwargs['pk'])
+        return self.request.user == photo.user
 
     def get_success_url(self):
         return reverse_lazy('photo-details', kwargs={'pk': self.object.pk})
@@ -48,7 +52,11 @@ class PhotoDetailsView(LoginRequiredMixin, DetailView):
 
 @login_required
 def photo_delete(request, pk: int):
-    Photo.objects.get(pk=pk).delete()
+    photo = Photo.objects.get(pk=pk)
+
+    if request.user == photo.user:
+        photo.delete()
+
     return redirect('home')
 
 
